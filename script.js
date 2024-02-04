9259057af8a1580d01d2fba109ed02d32dbb033f
khaletska <khaletska.e@gmail.com>
2024-02-04 22:26:24 +0200

    more handlers for phone

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
 const button = document.getElementById('no');
 
-button.addEventListener('mouseover', () => {
+const btnHandler = () => {
     const newX = Math.random() * (window.innerWidth - button.clientWidth);
     const newY = Math.random() * (window.innerHeight - button.clientHeight);
-
+    
     button.style.position = 'absolute';
     button.style.top = 0;
     button.style.left = 0;
     button.style.transform = `translate(${newX}px, ${newY}px)`;
-});
+}
+
+button.addEventListener('mouseover', btnHandler);
+button.addEventListener('mousedown', btnHandler);
+button.addEventListener('click', btnHandler);
